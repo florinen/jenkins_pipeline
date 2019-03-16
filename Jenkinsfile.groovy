b4a7eaa6a7aeb8c1cd2aa58d4b7fd11ce734cef7
florinen <florinen>
2019-03-16 11:56:29 -0500

    add 2 properties

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,6 @@
 node {
 
-     properties([parameters([string(defaultValue: '127.0.0.1', description: 'Give IP to host website', name: 'DEVIP', trim: true)])]), properties([pipelineTriggers([pollSCM('* * * * *')])])
-
+ properties([parameters([string(defaultValue: '127.0.0.1', description: 'Add ip', name: 'DEVIP', trim: true)]), pipelineTriggers([pollSCM('* * * * *')])])
 stage("Pull git"){
         git "git@github.com:florinen/website.git"
     }
